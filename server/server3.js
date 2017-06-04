0f8035a805314b547db6bcb68921b61bbf7b4fc3
chris victores <victobiz@yahoo.com>
2017-06-04 03:06:57 -0400

    add patch/ todos/:id

--- server/server3.js
+++ server/server3.js
@@ -1,10 +1,11 @@
 //this connects to the db
 
-var express = require('express');
+const _= require('lodash');
+const express = require('express');
 
 
 //converts json to object attach to request object
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
@@ -92,6 +93,31 @@ Todo.findByIdAndRemove(id).then((todo)=>{
 //remove todo by id
    //sucess or error if 400 with empty body
    //if sucess send doc back with 200
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
 });
 
 app.listen(port, ()=> {
